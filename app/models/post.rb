22815a04c34c2d8b05300a5262c91c4651fafe29
Jeremy Fox <foxjerem@gmail.com>
2014-08-18 11:58:46 +0100

    Added functionality to upload pictures

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+
+	has_attached_file :picture, 
+		styles: { medium: "300x300>"},
+		storage: :s3,
+		s3_credentials: {
+			bucket: 'instagram-clone-june',
+			access_key_id: Rails.application.secrets.aws_s3_access_key_id,
+			secret_access_key: Rails.application.secrets.aws_s3_secret_access_key
+		}
+
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+
 end
