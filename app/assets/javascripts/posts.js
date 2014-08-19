9b8413f05b0c3cc2a27caf4b108c3f3c23da9489
Jeremy Fox <foxjerem@gmail.com>
2014-08-19 17:54:24 +0100

    Added inputosaurus to aid tagging

---
+++ app/assets/javascripts/posts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$('#post_tag_list').inputosaurus({
+		width: '200px',
+		outputDelimiter : ' OR ',
+		parseHook : function(valArr){
+			return $.map(valArr, function(val) {
+				val = $.trim(val);
+				return /\s/.test(val) ? '"' + val + '"' : val;
+			});
+		},
+		change : function(ev) {
+			$('#post_tag_list_reflect').val(ev.target.value);
+		}
+	});
+});
