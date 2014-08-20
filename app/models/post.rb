ce09c5582f1e5af9c68b86cb178a39abf8ebbb78
Jeremy Fox <foxjerem@gmail.com>
2014-08-20 15:32:45 +0100

    Tweaked nav bar

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
 	validates :picture, attachment_presence: { message: 'You must attach a photo' }
 
 	has_attached_file :picture, 
-		styles: { medium: '1000x1000>', thumb: '100x100>'},
+		styles: { medium: '300x300>', thumb: '100x100>'},
 		storage: :s3,
 		s3_credentials: {
 			bucket: 'instagram-clone-june',
